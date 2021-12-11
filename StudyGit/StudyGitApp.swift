b4dd816dad33d780debd737e39d51f2ffb892ca0
chitomo12 <mrk3_12@yahoo.co.jp>
2021-12-11 16:22:15 +0900

    Initial Commit

---
+++ StudyGit/StudyGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyGitApp.swift
+//  StudyGit
+//
+//  Created by 福田正知 on 2021/12/11.
+//
+
+import SwiftUI
+
+@main
+struct StudyGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
